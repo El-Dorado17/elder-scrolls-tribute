6da5f490956b56836fa55cea429390da0089bd7b
Ely Dorado <2018dorado@gmail.com>
2024-05-25 13:32:27 -0500

    Home structured

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,18 @@
 //? components ==> views ==> App.js ==> index.js ==> Browser renders 
 
+//HOME JS
+/*
+
+WELCOME/Welcome
+
+<<<image>>>
+
+"This is a small passion project dedicated to The Elder Scrolls games
+to keep my skills sharp and learn more about TES universe and making
+a resource for others to learn as well.
+
+Look around, learn, explore, and let me know what you think!"
+*/
 
 import React from "react";
 
