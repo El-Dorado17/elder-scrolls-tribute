8d8e153f0fb086704ca90c4ecd87fd8507c80cd5
Ely Dorado <2018dorado@gmail.com>
2024-05-25 13:34:52 -0500

    Game view annotated

---
+++ src/components/games/Games.js
@@ -0,0 +1,15 @@
+//GAMES JS
+/*
+
+Banner for main games
+
+<<<image>>>   <<<image>>>   <<<image>>>   <<<image>>>   <<<image>>>   <<<image>>>   <<<image>>>
+
+
+
+Banner for DLC and misc games
+<<<image>>>   <<<image>>>   
+
+!CLICK TO EXPAND A BOX ABOUT EACH GAME?
+
+*/
