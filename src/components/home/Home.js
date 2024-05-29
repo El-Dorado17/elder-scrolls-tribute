952414ee869e482dde163b01b7ec8b3d3c648a72
Ely Dorado <2018dorado@gmail.com>
2024-05-28 20:07:45 -0500

    Home View Started

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -21,7 +21,16 @@ export const Home =()=>{
     return(
         <section>
             <div>
-                <p>test</p>
+                <h1>Welcome!</h1>
+
+                <br/>
+                <br/>
+                <img src="https://assets-prd.ignimgs.com/2021/08/19/elder-scrolls-skyrim-button-2017-1629409446732.jpg"/>
+                <br/>
+                <br/>
+                <h5>This is a small passion project dedicated to The Elder Scrolls games to keep my skills sharp and learn more about TES universe while making a resource for others to learn as well.
+
+Look around, learn, explore, and let me know what you think! -Ely</h5>
             </div>
         </section>
     )
