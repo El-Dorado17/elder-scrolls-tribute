67360dda9be45f2ac2aa993441481fd04fdd4087
Ely Dorado <2018dorado@gmail.com>
2024-05-29 19:45:09 -0500

    Navigation incorporated

--- src/components/games/Games.js
+++ src/components/games/Games.js
@@ -13,3 +13,26 @@ Banner for DLC and misc games
 !CLICK TO EXPAND A BOX ABOUT EACH GAME?
 
 */
+
+
+
+
+
+
+export const Games =()=>{
+
+    return(
+        <section>
+            <div>
+                <h1>Games</h1>
+
+                <br/>
+                <br/>
+                {/* <img src="https://assets-prd.ignimgs.com/2021/08/19/elder-scrolls-skyrim-button-2017-1629409446732.jpg"/> */}
+                <br/>
+                <br/>
+                <h5>The games order are as follows:</h5>
+            </div>
+        </section>
+    )
+}
--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -6,6 +6,7 @@
 
 import React from "react"
 import { Home } from "../home/Home.js"
+import { Games } from "../games/Games.js"
 
 // import { Contact } from "../contact/Contact.js"
 import { Route, Routes } from "react-router-dom"
@@ -14,11 +15,11 @@ export const ApplicationViews = () => {
     return <>
         <Routes>        
             <Route path="/" element={<Home/>}/>
-            {/* <Route path="/Resume" element={<Games/>}/>
+            <Route path="/Resume" element={<Games/>}/>
             <Route path="/About" element={<History/>}/>
             <Route path="/Projects" element={<Gallery/>}/>
             <Route path="/Photos" element={<Quiz/>}/>
-            <Route path="/Contact" element={<Contact/>}/> */}
+            <Route path="/Contact" element={<Contact/>}/>
         </Routes>
     </>
 }
