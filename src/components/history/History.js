19946b116de2c9641fa06842cb45c2663d75ddb4
Ely Dorado <2018dorado@gmail.com>
2024-05-29 19:47:56 -0500

    History module

--- src/components/history/History.js
+++ src/components/history/History.js
@@ -6,17 +6,10 @@ export const History =()=>{
     return(
         <section>
             <div>
-                <h1>Games</h1>
+                <h1>History</h1>
+                <br/>
+                <br/>
 
-                <br/>
-                <br/>
-                {/* <img src="https://assets-prd.ignimgs.com/2021/08/19/elder-scrolls-skyrim-button-2017-1629409446732.jpg"/> */}
-                <br/>
-                <br/>
-                <h5>The games order are as follows:</h5>
-                <br/>
-                <br/>
-                <h5>Side Games and DLC's:</h5>
             </div>
         </section>
     )
