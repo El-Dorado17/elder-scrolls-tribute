2f9434384ff1d1bdfb1f8feba09c5893cf6826ce
Ely Dorado <2018dorado@gmail.com>
2024-05-29 19:50:56 -0500

    views navigations

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -8,18 +8,20 @@ import React from "react"
 import { Home } from "../home/Home.js"
 import { Games } from "../games/Games.js"
 import { History } from "../history/History.js"
+import { Gallery } from "../gallery/Gallery.js"
+import { Contact } from "../contact/Contact.js"
 
-// import { Contact } from "../contact/Contact.js"
+
 import { Route, Routes } from "react-router-dom"
 
 export const ApplicationViews = () => {
     return <>
         <Routes>        
             <Route path="/" element={<Home/>}/>
-            <Route path="/Resume" element={<Games/>}/>
-            <Route path="/About" element={<History/>}/>
-            <Route path="/Projects" element={<Gallery/>}/>
-            <Route path="/Photos" element={<Quiz/>}/>
+            <Route path="/Games" element={<Games/>}/>
+            <Route path="/History" element={<History/>}/>
+            <Route path="/Gallery" element={<Gallery/>}/>
+            <Route path="/Quiz" element={<Quiz/>}/>
             <Route path="/Contact" element={<Contact/>}/>
         </Routes>
     </>
