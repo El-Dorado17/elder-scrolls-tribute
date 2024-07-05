1fa7a6099d14481dc1549d41b5bbcc405ebb1628
Ely Dorado <2018dorado@gmail.com>
2024-07-05 11:03:16 -0500

    CSS home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -20,17 +20,20 @@ Look around, learn, explore, and let me know what you think!"
 */
 
 import React from "react";
+import "./Home.css"
 
 export const Home =()=>{
 
     return(
         <section>
-            <div>
-                <h1>Welcome! Drem Yol Lok</h1>
+            <div className="body">
+                <h1>Welcome! <br /> Drem Yol Lok</h1>
 
                 <br/>
                 <br/>
-                <img src="https://assets-prd.ignimgs.com/2021/08/19/elder-scrolls-skyrim-button-2017-1629409446732.jpg" alt="Skyrim-cover"/>
+                <div className="image-contaner">
+                    <img className="image1" src="https://assets-prd.ignimgs.com/2021/08/19/elder-scrolls-skyrim-button-2017-1629409446732.jpg" alt="Skyrim-cover"/>
+                </div>
                 <br/>
                 <br/>
                 <h5>This is a small passion project dedicated to The Elder Scrolls games to keep my skills sharp and learn more about TES universe while making a resource for others to learn as well.
