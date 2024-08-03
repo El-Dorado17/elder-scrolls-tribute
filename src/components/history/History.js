70a8ba90bb97f41abd7d49b77cec749c5fea7d23
Ely Dorado <2018dorado@gmail.com>
2024-08-03 07:39:14 -0500

    HISTORY added

--- src/components/history/History.js
+++ src/components/history/History.js
@@ -1,4 +1,63 @@
+/*
 
+HISTORY 
+The Elder Scrolls timeline uses centuries to measure "Eras"
+
+NOTE
+I will do my best to concisely summarize the eras as they are a DEEP chasm of lore. For an in-depth read on the lore, take a look at  <link> Fandom <link/> as I gathered most of my information from there. 
+
+2E 582
+    ESO
+    Imperial City (DLC)
+    Orsinium (DLC)
+    Greymoor (DLC)
+    Blackwood (DLC)
+
+3E 389
+    Elder Scrolls Arena
+
+3E 405
+    Elder Scrolls II: Daggerfall
+
+3E 427
+    Morrowind
+    Bloodmoon (DLC)
+    Tribunal (DLC)
+
+3E 433
+    Oblivion
+    Knights of the Nine (DLC)
+    Shivering Isles (DLC)
+
+4E 201
+    Skyrim
+    Dawnguard (DLC)
+    Dragonborn (DLC)
+
+ERAS
+	•	PREHISTORIC
+	•	DAWN
+	•	MERETHIC
+
+
+1ST (30 Centuries)
+1E 0 - 1E 2920
+
+2ND (9 Centuries)
+2E 0 - 2E 896
+
+3RD (5 Centuries)
+3E 0  - 3E 433
+
+4TH (3 Centuries)
+4E 0 - 4E 201
+
+TOTAL 4,450 YEARS
+
+
+
+
+*/
 
 
 export const History =()=>{
