ab584f1db44f5bc420848d66986a8421c1ea99ee
Ely Dorado <2018dorado@gmail.com>
2024-08-03 07:40:26 -0500

    Game outline added

--- src/components/games/Games.js
+++ src/components/games/Games.js
@@ -12,6 +12,54 @@ Banner for DLC and misc games
 
 !CLICK TO EXPAND A BOX ABOUT EACH GAME?
 
+
+
+
+
+GAMES
+
+The Elder Scrolls: Arena
+March 25, 1994
+
+The Elder Scrolls II: Daggerfall
+September 20, 1996
+
+An Elder Scrolls Legend: Battlespire
+November 30, 1997
+
+The Elder Scrolls Adventures: Redguard
+October 31, 1998
+
+The Elder Scrolls III: Morrowind
+May 1, 2002
+
+The Elder Scrolls Travels
+Stormhold 08/1/03
+Dawnstar 08/26/04
+Shadowkey 11/11/04
+
+The Elder Scrolls IV: Oblivion
+March 20, 2006
+
+The Elder Scrolls V: Skyrim
+November 11, 2011
+
+The Elder Scrolls Online
+April 4, 2014
+
+The Elder Scrolls Legends
+March 9, 2017
+
+The Elder Scrolls Blades
+March 27, 2019
+
+The Elder Scrolls Castles
+December 25, 2024?
+
+The Elder Scrolls VI
+?
+
+
 */
 
 
