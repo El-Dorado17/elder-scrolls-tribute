8fb4f6e4776912c7b44d3eba85aaec965a80e364
Ely Dorado <2018dorado@gmail.com>
2024-08-27 17:12:33 -0500

    games added chronologically

--- src/components/games/Games.js
+++ src/components/games/Games.js
@@ -81,6 +81,49 @@ export const Games =()=>{
                 <br/>
                 <h5>The games order are as follows:</h5>
                 <br/>
+                <ul>
+                    <li> The Elder Scrolls: Arena
+March 25, 1994 </li>
+
+<li> The Elder Scrolls II: Daggerfall
+September 20, 1996 </li>
+
+<li> An Elder Scrolls Legend: Battlespire
+November 30, 1997 </li>
+
+<li> The Elder Scrolls Adventures: Redguard
+October 31, 1998 </li>
+
+<li> The Elder Scrolls III: Morrowind
+May 1, 2002 </li>
+
+<li> The Elder Scrolls Travels </li>
+    <ol>
+        <li> Stormhold 08/1/03 </li>
+        <li> Dawnstar 08/26/04 </li>
+        <li> Shadowkey 11/11/04</li>
+    </ol>
+
+<li> The Elder Scrolls IV: Oblivion
+March 20, 2006 </li>
+
+<li> The Elder Scrolls V: Skyrim
+November 11, 2011 </li>
+
+<li> The Elder Scrolls Online
+April 4, 2014 </li>
+
+<li> The Elder Scrolls Legends
+March 9, 2017 </li>
+
+<li> The Elder Scrolls Blades
+March 27, 2019 </li>
+
+<li> The Elder Scrolls Castles
+December 25, 2024? </li>
+
+<li> The Elder Scrolls VI </li>
+                </ul>
                 <br/>
                 <h5>Side Games and DLC's:</h5>
             </div>
